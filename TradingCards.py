c1e61d4642fa621118a3b37165f19539c64758c4
nbness2 <nbness1337@gmail.com>
2016-03-03 03:45:08 -0700

    Expanded on editTheme()
    You can now actually edit the theme, but for now it is only editing,
    adding or deleting card names. When I get some more slow time at work or
    at home, I'll add in some things. editing tiers will be first priority

--- TradingCards.py
+++ TradingCards.py
@@ -189,7 +189,79 @@ def createTheme():
 def editTheme():
     isTheme = False
     themeList = listdir('themes')
-    editThemeName = inpConf('{0}\nInput the name of the theme you are editing: '.format(themeList))c cv
+    themeStr = ', '.join(themeList)[0:]
+    validChoice = False
+    while not isTheme:
+        themeName = inpConf('{0}\nInput the name of the theme you are editing: '.format(themeStr)).capitalize()
+        if themeName in themeList:
+            isTheme = True
+        else:
+            print('Invalid theme:', themeName)
+    fileDir = 'themes/'+themeName+'/'
+    while True:
+        editChoice = inpConf('Editing (C)ard Names or (T)iers (-- to quit): ').lower()
+
+        if editChoice == 'c'[0]:
+            with open(fileDir+'cnames.txt', 'r') as cNameFile:
+                cardNames =  [name.strip() for name in cNameFile.readlines()]
+            cardNamesL = [name.lower() for name in cardNames]
+            while True:
+                cardNamesL = [name.lower() for name in cardNames]
+                cardNameStr = ', '.join(cardNames)
+                validChoice = False
+                cardChoice = inpConf('(E)diting, (A)dding, or (D)eleting a card name (-- to quit): ').lower()
+                if cardChoice[0] == 'e':
+                    print(cardNameStr)
+                    nameChoice = inpConf('Which name are you editing: ')
+                    validChoice = True
+                elif cardChoice[0] == 'a':
+                    nameChoice = inpConf('Which name are you adding: ')
+                    validChoice = True
+                elif cardChoice[0] == 'd':
+                    print(cardNameStr)
+                    nameChoice = inpConf('Which name are you deleting: ')
+                    validChoice = True
+                elif cardChoice == '--':
+                    validChoice = False
+                    break
+                else:
+                    validChoice = False
+                if validChoice:
+                    if cardChoice == 'a':
+                        if not nameChoice.lower() in cardNamesL:
+                            cardNames.append(nameChoice)
+                        else:
+                            print('Card name already exists!')
+                    elif nameChoice.lower() in cardNamesL:
+                        nameIndex = cardNamesL.index(nameChoice)
+                        if cardChoice == 'e':
+                            cardNames[nameIndex] = inpConf('Input card name: ')
+                        elif cardChoice == 'd':
+                            del cardNames[nameIndex]
+                        else:
+                            print('editTheme error', cardNames, nameChoice)
+                    else:
+                        print('Name',nameChoice,'not found.')
+                else:
+                    print('Invalid input:', cardChoice)
+
+            with open(fileDir+'cnames.txt', 'w') as cNameFile:
+                for idx, cName in enumerate(cardNames):
+                    if idx < len(cardNames)-1:
+                        cNameFile.write(cName+'\n')
+                    else:
+                        cNameFile.write(cName)
+
+        elif editChoice[0] == 't':
+            with open(fileDir+'tnames.txt', 'r') as tNameFile:
+                tierNames = tNameFile.readlines()
+            with open(fileDir+'tchances.txt', 'r') as tChanceFile:
+                tierChances = tChanceFile.readlines()
+            tierChoice = inpConf('Editing Tier (N)ames or Tier (C)hances: ')[0].lower()
+        elif editChoice == '--':
+            break
+        else:
+            print('Invalid input:', editChoice)
 
 def createPack():
     baseTheme = 'Basic'
