cea5106fb15f8b1f934b806a7d6fdeea25580111
nbness2 <nbness1337@gmail.com>
2016-03-04 13:59:20 -0700

    Prettify(Code) pt.2
    Forgot I had tests.py for some reason.

--- tests.py
+++ tests.py
@@ -1,17 +1,23 @@
 import TradingCards as tc
 
 def test_packs():
+
     testInputList = ['50%','30%','10%','5%','3%','1.5%','.5%']
     testWeightList = [.5,.3,.1,.05,.03,.015,.005]
     results = {x:0 for x in testInputList}
     print('Opening packs')
+
     for pack in tc.packs.values():
         cards = pack.openPack()
+
         for card in cards:
             print('{} {} from {}'.format(card.cardTier, card.cardName, pack.packName))
+
     print('Testing 10000 iterations of RNG')
+
     for x in range(10000):
         results[tc.weightchoice(testInputList, testWeightList)]+=1
+
     print(results)
 
 
