1b324ef4d89c2e1fccea481689e09fd0f54acfdd
nbness2 <nbness1337@gmail.com>
2016-03-08 05:05:57 -0700

    [+-] Split up tests in tests.py
    Split up tests so you don't have to do all of the tests if you only want
    to test one thing. Will probably add more tests later.

--- tests.py
+++ tests.py
@@ -1,27 +1,16 @@
 import TradingCards as tc
 
-def run_tests():
+def runTests():
+    rngTest()
+    packTest()
+
+def rngTest():
 
     testInputList = ['50','30','10','5','3','1.5','.5']
     perfectResults = {x+'%':int(float(x)*100) for x in testInputList}
     testInputList = [x+'%' for x in testInputList]
     testWeightList = [.5,.3,.1,.05,.03,.015,.005]
     results = {x:0 for x in testInputList}
-    print('Opening packs')
-    print('Testing opening packs. Opening 10 rounds of packs.\n')
-
-    for pack in tc.packs.values():
-
-        print(pack.packName, pack.cardAmt, pack.themeCardChance, pack.maxThemed)
-
-        for x in range(10):
-
-            print('\tPack', x+1)
-            cards = pack.openPack()
-
-            for card in cards:
-                print('\t\t', card.cardTier, card.cardName)
-
     print('\nTesting 10000 iterations of 1-draw weighted choice rng~~')
 
     for test in range(10000):
@@ -34,6 +23,7 @@ def run_tests():
 
     for test in range(1000):
         resultList = tc.weightchoice(testInputList, testWeightList, draws = 10)
+
         for result in resultList:
             results[result] += 1
 
@@ -43,6 +33,7 @@ def run_tests():
 
     for test in range(100):
         resultList = tc.weightchoice(testInputList, testWeightList, draws = 100)
+
         for result in resultList:
             results[result] += 1
 
@@ -51,6 +42,22 @@ def run_tests():
     print('\nPerfect results~~')
     print(perfectResults)
 
+def packTest():
+
+    print('Opening packs')
+    print('Testing opening packs. Opening 10 rounds of packs.\n')
+
+    for pack in tc.packs.values():
+
+        print(pack.packName, pack.cardAmt, pack.themeCardChance, pack.maxThemed)
+
+        for x in range(10):
+            print('\tPack', x+1)
+            cards = pack.openPack()
+
+            for card in cards:
+                print('\t\t', card.cardTier, card.cardName)
+
 if __name__ == '__main__':
-    run_tests()
+    runTests()
     input()
