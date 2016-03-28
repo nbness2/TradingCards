d481c64cd3755a415ef07081f48f7b51e01ac053
nbness2 <nbness1337@gmail.com>
2016-03-28 03:38:31 -0700

    [++] Creating GUI
    Right now it is just a test and has nothing to do with TCG, I am just
    playing around with pygame trying to figure out how it works before I go
    too deep into this.

---
+++ TCGui.py
@@ -0,0 +1,83 @@
+import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+crashed = False
+img = pygame.image
+
+colors = {'black' : (0,0,0), 'white' : (255,255,255), 'red' : (255,0,0), 'green' : (0,255,0), 'blue' : (0,0,255),
+          'purple' : (255,0,255), 'yellow' : (255,255,0)}
+
+disWidth = 1000
+disHeight = 750
+gameDis = pygame.display.set_mode((disWidth, disHeight))
+pygame.display.set_caption('TCGui')
+cardImg = img.load('BasicCard.png')
+cardX, cardY = disWidth * .35, disHeight * .35
+xchange, ychange = 0, 0
+
+
+class Button:
+
+    def __init__(self, buttonColor, sizeX, sizeY, posX, posY, text = '', font = 'freesansbold.ttf', fontColor = 'black'):
+        self.text = text
+        self.buttonColor = buttonColor
+        self.sizeX, self.sizeY = sizeX, sizeY
+        self.posX, self.posY = posX, posY
+        self.font = font
+        self.fontColor = fontColor
+
+    def draw(self):
+        # x, y, width, height
+        textX = self.posX+(self.sizeX/2)
+        textY = self.posY+(self.sizeY/2)
+        textSize = self.sizeX / 5.3
+        pygame.draw.rect(gameDis, colors[self.buttonColor], (self.posX, self.posY, self.sizeX, self.sizeY))
+        dispText(self.text, textX, textY, self.font, textSize, colors[self.fontColor])
+
+testButton = Button('green', 200, 50, 300, 300, 'testButton')
+
+
+def dispText(msg, textX, textY, font = 'freesansbold.ttf', fontSize = 20, textColor = (0,0,0), bgColor = None):
+    msgFont = pygame.font.Font(str(font), int(fontSize))
+    textSurface = msgFont.render(msg, True, textColor, bgColor)
+    textRect = textSurface.get_rect()
+    textRect.center = (textX, textY)
+    gameDis.blit(textSurface, textRect)
+
+def drawCard(x, y):
+    gameDis.blit(cardImg, (x, y))
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                xchange = -1
+            elif event.key == pygame.K_RIGHT:
+                xchange = 1
+
+            if event.key == pygame.K_UP:
+                ychange = 1
+            elif event.key == pygame.K_DOWN:
+                ychange = -1
+
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                xchange = 0
+            if event.key in (pygame.K_UP, pygame.K_DOWN):
+                ychange = 0
+
+    cardX += xchange
+    cardY += -ychange
+    gameDis.fill(colors['white'])
+    dispText('abc123', 300, 300,'freesansbold.ttf', 80, colors['blue'], colors['yellow'])
+    drawCard(cardX, cardY)
+    testButton.draw()
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
