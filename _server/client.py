f71125be7703301506882ed6cd219f2bae0262ef
nbness2 <nbness1337@gmail.com>
2016-04-02 02:57:15 -0700

    [++] Basic echo client
    This is a basic echo client with 3 commands. 0x00 is to print data, not
    accept input, 0x01 is to print data and accept input, 0x09 is to break
    the echo loop and close the connection.

---
+++ _server/client.py
@@ -0,0 +1,34 @@
+import socket
+
+def client(host, port):
+    # Talk to server until disconnected.
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        while True:
+            reply = s.recv(256).decode()[:256]
+            if len(reply) == 0:
+                reply = '9'
+            command = int(reply[0])
+            data = reply[1:]
+            if command == 0x00:
+                print(data)
+                s.send('1'.encode())
+            elif command == 0x01:
+                valid = False
+                while not valid:
+                    message = input(data)
+                    if len(message):
+                        valid = True
+                s.send(message.encode())
+            elif command == 0x09:
+                break
+        s.close()
+    except Exception as e:
+        s.close()
+
+if __name__ == '__main__':
+    host = '127.0.0.1'
+    port = 1337
+    while True:
+        client(host, port)
