7f8d9bc0d80323a4dd02d227e1a6cc555366a2c2
nbness2 <nbness1337@gmail.com>
2016-04-04 03:44:06 -0700

    [++] Added errors to registering.
    Now the user will be sent all of the errors they had in registration, if
    they had any.

--- TCGServer.py
+++ TCGServer.py
@@ -18,18 +18,29 @@ class UserHandler(socketserver.BaseRequestHandler):
         socket = self.request
         allValid = False
         uemail, upass, uname = ('','','')
+        paramchecks = {}
         while not allValid:
-            uname = sendRecv(socket, '\nMin 4 characters, max 16 characters.\nEnter desired username: ', recvsize = 16)
+            estring = ''
+            if len(paramchecks):
+                for param in ['Username', 'Password', 'Email']:
+                    if len(paramchecks[param]):
+                        estring += param+': '
+                    for error in paramchecks[param]:
+                        estring += error+', '
+                    estring = estring[:-2]+'\n'
+                sendRecv(socket, estring, 'p', 1)
+            uname = sendRecv(socket, 'Min 4 characters, max 16 characters.\nEnter desired username: ', recvsize = 16)
             upass = sendRecv(socket, '\nMin 8 characters, max 32 characters. Must have at least 1 letter and number.\nCannot symbols.\nEnter password: ', recvsize = 32)
             uemail = sendRecv(socket, '\nYour activation code will be sent to this email.\nEnter a valid email: ', recvsize= 64)
-            if False not in checkupe(uname, upass, uemail):
+            print()
+            paramchecks = checkupe(uname, upass, uemail)
+            if type(paramchecks) == bool:
                 allValid = True
         actCode = pyhash.Md5(pyrand.randstring(8)).hexdigest[:8]
         emessage = 'Dear {0}, Thank you for registering your account with pyTCG! Your activation code is:\n{1}'.format(uname, actCode)
         pyemail.sendEmail(uemail, emessage, 'pyTCG activation code',
                           email, emailpass, 'smtp.gmail.com')
         print('email sent to', uemail)
-
 
 
 class TestHandler(socketserver.BaseRequestHandler):
@@ -43,20 +54,40 @@ class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass):
         socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
 
-def sendRecv(socket, sendmsg, type = 'i', recvsize = 64):
+def sendRecv(socket, sendmsg, stype = 'i', recvsize = 64):
     # Sends encoded data + command, returns decoded receive data
     # p, 0x00 = no input
     # i, 0x01 = input
     commands = {'p' : 0x00, 'i' : 0x01}
-    sendData = str(commands[type])+sendmsg
+    sendData = str(commands[stype])+sendmsg
     socket.send(sendData.encode())
-    if type == 'i':
+    if stype == 'i':
         recvData = socket.recv(recvsize).decode()[:recvsize]
         return recvData
     socket.recv(recvsize)[:1]
 
 def checkupe(username, password, email):
-    return (regrules.checkUsername(username), regrules.checkEmail(email), regrules.checkPassword(password))
+    faults = {'Username' : [], 'Password' : [], 'Email' : []}
+    fullPass = True
+
+    unamec = regrules.checkUsername(username)
+    if len(unamec):
+        fullPass = False
+        faults['Username'].extend(unamec)
+
+    pwordc = regrules.checkPassword(password)
+    if len(pwordc):
+        fullPass = False
+        faults['Password'].extend(pwordc)
+
+    emailc = regrules.checkEmail(email)
+    if type(emailc) != bool:
+        fullPass = False
+        faults['Email'].append(emailc)
+
+    if fullPass:
+        return True
+    return faults
 
 if __name__ == "__main__":
     echo = SimpleServer((HOST, PORT), UserHandler)
--- regrules.py
+++ regrules.py
@@ -1,26 +1,34 @@
 def checkUsername(username):
     #username length between 4 and 16
-    #username must have at least 1 number and 1 letter
-    if not 4 < len(username) < 16 or not username.isalnum():
-        return False
-    return True
+    #username cannot contain symbols
+    faults = []
+    if not 4 < len(username) < 16:
+        faults.append('too long\short')
+    if not username.isalnum():
+        faults.append('can only contain alphanumeric characters')
+    return faults
 
 def checkPassword(password):
     #password length between 8 and 32
     #password must have at least 1 number and 1 letter
     #password cannot contain symbols
-    if not 8 <= len(password) <= 32 or not password.isalnum() or password.isalpha() or password.isnumeric():
-        return False
-    return True
+    faults = []
+    if not 8 <= len(password) <= 32:
+        faults.append('too long\short')
+    if not password.isalnum():
+        faults.append('can only contain alphanumeric characters')
+    if password.isalpha() or password.isnumeric():
+        faults.append('must have at least 1 letter and number')
+    return faults
 
 def checkEmail(email):
     #must look like an email. If they don't get email it is because they entered wrong.
     if '@' not in email:
-        return False
+        return ('invalid email')
     uname, domain = email.split('@')
     if '.' not in domain:
-        return False
+        return ('invalid email')
     domain, ext = domain.split('.')
     if len(domain) < 2 or len(ext) <= 1:
-        return False
+        return ('invalid email')
     return True
