e626cc96df45769ae986122f17445fedfc90c1d9
nbness2 <nbness1337@gmail.com>
2016-04-07 02:58:53 -0600

    [+-] changed name of sendEmail to send_email

--- TCGServer.py
+++ TCGServer.py
@@ -1,50 +1,9 @@
 import socketserver, sys, regrules
 import emailinfo
-from queue import Queue
 from modules import pyrand, pyemail, pyhash
+from queue import Queue
+from threading import Thread
 from os import walk
-
-regQueue = Queue()
-
-email = emailinfo.email
-emailpass = emailinfo.password
-smtpaddr = emailinfo.smtp
-
-HOST = ''
-PORT = 1337
-
-class UserHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        self.register()
-        self.request.close
-
-    def register(self):
-        socket = self.request
-        allValid = False
-        uemail, upass, uname = ('', '', '')
-        paramchecks = {}
-        while not allValid:
-            if len(paramchecks):
-                estring = errstr(paramchecks, ['Username', 'Password', 'Email'])
-                sendRecv(socket, estring, 'p', 1)
-            uname = sendRecv(socket, 'Min 4 characters, max 16 characters.\nEnter desired username: ', recvsize = 16)
-            upass = sendRecv(socket, '\nMin 8 characters, max 32 characters. Must have at least 1 letter and number.\nCannot symbols.\nEnter password: ', recvsize = 32)
-            uemail = sendRecv(socket, '\nYour activation code will be sent to this email.\nEnter a valid email: ', recvsize = 64)
-            paramchecks = checkupe(uname, upass, uemail)
-            if type(paramchecks) == bool:
-                allValid = True
-                phash = pyhash.Sha384(upass).hexdigest
-        del upass, paramchecks, allValid, estring
-        ehash = pyhash.Sha384(uemail.lower()).hexdigest
-        actCode = pyhash.Md5(pyrand.randstring(8)).hexdigest[:8]
-        regQueue.put(writeuser((uname, False, actCode, phash, ehash)))
-        emessage = 'Dear {0}, Thank you for registering your account with pyTCG! Your activation code is:\n{1}'.format(uname, actCode)
-        pyemail.sendEmail(uemail, emessage, 'pyTCG activation code', email, emailpass, 'smtp.gmail.com')
-
-
-class TestHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        self.request.close
 
 
 class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -55,7 +14,98 @@ class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
         socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
 
 
-def sendRecv(socket, sendmsg, stype = 'i', recvsize = 64):
+class UserHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        try:
+            self.register()
+            self.request.close()
+        except:
+            pass
+
+    def login(self):
+        socket = self.request
+        username = send_receive(socket, 'Username: ', recvsize = 16)
+        passhash = pyhash.Md5(send_receive(socket, 'Password: ', recvsize = 32)).hexdigest
+        activated = is_activated(username)
+        while not activated:
+            self.activate()
+
+
+    def activate(self):
+        socket = self.request
+        pass
+
+
+    def register(self):
+        socket = self.request
+        allValid = False
+        useremail, password, username = ('', '', '')
+        paramchecks = {}
+        while not allValid:
+            if len(paramchecks):
+                estring = err_str(paramchecks, ['Username', 'Password', 'Email'])
+                send_receive(socket, estring, 'p', 1)
+            username = send_receive(socket, 'Min 4 characters, max 16 characters.\nEnter desired username: ', recvsize = 16)
+            password = send_receive(socket, '\nMin 8 characters, max 32 characters. Must have at least 1 letter and number.\nCannot symbols.\nEnter password: ', recvsize = 32)
+            useremail = send_receive(socket, '\nYour activation code will be sent to this email.\nEnter a valid email: ', recvsize = 64)
+            paramchecks = check_all(username, password, useremail)
+            if type(paramchecks) == bool:
+                allValid = True
+        passhash = pyhash.Sha384(password).hexdigest
+        del password, paramchecks, allValid
+        ehash = pyhash.Sha384(useremail.lower()).hexdigest
+        activation_code = pyhash.Md5(pyrand.randstring(16)).hexdigest[::4]
+        regQueue.put(write_user(username, (False, activation_code, passhash, ehash)))
+        emessage = 'Dear {0}, Thank you for registering your account with pyTCG! Your activation code is:\n{1}'.format(username, activation_code)
+        Email(useremail, emessage, 'pyTCG activation code', email, emailpass, 'smtp.gmail.com').start()
+        del username, activation_code
+        send_receive(socket, 'Your account has been registered, and an activation code has been sent to your email.', 'p', 0)
+
+
+class TestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.request.close()
+
+
+class SessionContainer(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.sessions = {}
+
+    def addSession(self, sessname):
+        self.sessions[sessname] = pyhash.Md5(sessname+pyrand.randstring(2))[:16]
+
+    def removeSession(self, sessname):
+        del self.sessions[sessname]
+
+
+class QueueWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            try:
+                if not self.queue.empty():
+                    self.queue.get()
+            except:
+                pass
+
+class Email(Thread):
+    def __init__(self, sendTo, text, subject, loginName, loginPass, ServerAddr):
+        Thread.__init__(self)
+        self.sendTo, self.subject, self.text = sendTo, subject, text
+        self.loginName, self.loginPass = loginName, loginPass
+        self.ServerAddr = ServerAddr
+
+    def run(self):
+        pyemail.send_email(self.sendTo, self.text, self.subject, self.loginName, self.loginPass, self.ServerAddr)
+        print('sent')
+
+
+
+def send_receive(socket, sendmsg, stype = 'i', recvsize = 64):
     # Sends encoded data + command, returns decoded receive data
     # p, 0x00 = no input
     # i, 0x01 = input
@@ -68,7 +118,7 @@ def sendRecv(socket, sendmsg, stype = 'i', recvsize = 64):
     socket.recv(recvsize)[:1]
 
 
-def errstr(errdict, paramorder = ()):
+def err_str(errdict, paramorder = ()):
     estring = ''
     for param in paramorder if len(paramorder) else errdict.keys():
         if len(errdict[param]):
@@ -79,21 +129,21 @@ def errstr(errdict, paramorder = ()):
     return estring+'\n'
 
 
-def checkupe(username, password, email):
+def check_all(username, password, email):
     faults = {'Username' : [], 'Password' : [], 'Email' : []}
     fullPass = True
 
-    unamec = regrules.checkUsername(username)
-    if len(unamec):
+    usernamec = regrules.check_username(username)
+    if len(usernamec):
         fullPass = False
-        faults['Username'].extend(unamec)
+        faults['Username'].extend(usernamec)
 
-    pwordc = regrules.checkPassword(password)
-    if len(pwordc):
+    passwordc = regrules.check_password(password)
+    if len(passwordc):
         fullPass = False
-        faults['Password'].extend(pwordc)
+        faults['Password'].extend(passwordc)
 
-    emailc = regrules.checkEmail(email)
+    emailc = regrules.check_email(email)
     if type(emailc) != bool:
         fullPass = False
         faults['Email'].append(emailc)
@@ -102,39 +152,52 @@ def checkupe(username, password, email):
         return True
     return faults
 
-def readusernames(dirname = 'users'):
+
+def read_usernames(dirname = 'users'):
     return [username[:-4] for username in walk(dirname).__next__()[2]]
 
-def writeuser(details, userdir = 'users/'):
-    uname = details[0]+'.usr'
-    details = details[1:]
-    with open(userdir+uname, 'w') as ufile:
+
+def write_user(username, details, userdir = 'users/'):
+    username+='.usr'
+    with open(userdir+username, 'w') as ufile:
         for detail in details:
             detail = str(detail)+'\n'
             ufile.write(detail)
     return True
 
-def readuser(username, userdir = 'users/'):
-    username = username+'.usr'
-    user = {'activated':None, 'actcode':None, 'passhash':None, 'emailhash':None}
-    with open(userdir+username, 'r') as ufile:
-        details = [detail.strip() for detail in ufile.readlines()]
-    user['activated'], user['actcode'], user['passhash'], user['emailhash'] = details
-    return user
 
-def queueworker(queue):
-    while True:
-        try:
-            if not queue.empty():
-                queue.get()
-        except:
-            pass
+def read_user(username, userdir = 'users/'):
+    username += '.usr'
+    #user = {'activated':None, 'actcode':None, 'passhash':None, 'emailhash':None}
+    with open(userdir+username, 'r') as ufile:
+        details = tuple([detail.strip() for detail in ufile.readlines()])
+    #user['activated'], user['actcode'], user['passhash'], user['emailhash'] = details
+    return details
+
+def is_activated(username):
+    if read_user(username)[0]:
+        return True
+    return False
+
+regQueue = Queue()
+loginQueue = Queue()
+#sessions = {'username':'sessionid'}
+
+email = emailinfo.email
+emailpass = emailinfo.password
+smtpaddr = emailinfo.smtp
+
+HOST = ''
+PORT = 1337
 
 
 if __name__ == "__main__":
     server = SimpleServer((HOST, PORT), UserHandler)
     try:
+        regqworker = QueueWorker(regQueue)
+        #logqworker = QueueWorker(loginQueue)
+        regqworker.start()
+        #logqworker.start()
         server.serve_forever()
-        queueworker(regQueue)
     except KeyboardInterrupt:
         sys.exit(0)
