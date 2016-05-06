622ec1c82a97e54624f5d8c6b3376d9428f5a427
nbness2 <nbness1337@gmail.com>
2016-05-06 03:27:16 -0600

    [++] My own queues
    Queues are pretty much just a list, so I could just use that, but saying
    that I use Queues sounds SO SOPHISTICATED. Plus less overhead...?

--- TCGServer.py
+++ TCGServer.py
@@ -1,7 +1,6 @@
 import sys, time, socketserver
 import emailinfo, regrules, TCGMain
 from modules import pyrand, pyemail, pyhash
-from queue import Queue
 from threading import Thread
 from os import walk
 
@@ -12,6 +11,36 @@ class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
     def __init__(self, server_address, request_handler):
         socketserver.TCPServer.__init__(self, server_address, request_handler)
+
+
+class Queue:
+    def __init__(self, qtype='f', maxsize=0):
+        if qtype.lower()[0] in 'lf':
+            self.qtype = qtype
+        else:
+            raise TypeError('qtype must be (l) or (f)')
+        if maxsize < 0:
+            self.maxsize = 0
+        else:
+            self.maxsize = maxsize
+        self.queue = []
+
+    def put(self, item):
+        if self.maxsize > 0:
+            if len(self.queue) == self.maxsize:
+                raise ValueError('Queue is full')
+        self.queue.append(item)
+
+    def get(self):
+        if self.qtype == 'f':
+            return self.queue.pop(0)
+        elif self.qtype == 'l':
+            return self.queue.pop(len(self.queue)-1)
+        else:
+            raise TypeError('Type must be (l) or (f)')
+
+    def empty(self):
+        return not bool(self.queue)
 
 
 class UserHandler(socketserver.BaseRequestHandler):
