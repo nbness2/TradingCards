9f5af65bcb64bfaddd6c48ef8638ff3a914ed8c4
nbness2 <nbness1337@gmail.com>
2016-06-03 03:12:08 -0600

    [CHANGE] Double Linked List
    DoubleLinkedList now inherits from SingleLinkedList, as they are very
    compatible. Most of these function changes are changed to introduce the
    benefits of DoubleLinkedList over SingleLinkedList, namely O(1)
    optimized access times to either end of the list.
    
    I'm sure I can clean up _remove, but for now I must sleep.

--- modules/pyqueue.py
+++ modules/pyqueue.py
@@ -108,51 +108,54 @@ class SingleLinkedList:
         return (self[item], self._remove(item))[0]
 
 
+class DoubleLinkedList(SingleLinkedList):
+    structname = 'double linked list'
 
-class DoubleLinkedList:
-    head = None
-    end = None
+    def __init__(self, iterable):
+        SingleLinkedList.__init__(self, iterable)
 
-    def __init__(self, iterable=[]):
-        self.extend(iterable)
-
-    def __str__(self):
-        if len(self) < 1:
-            return '[]'
-        current_ref = self.head
-        retstr = '['
-        while current_ref:
-            retstr = retstr + repr(current_ref.data) + ', '
-            current_ref = current_ref.next_ref
-        return retstr[:-2]+']'
-
-    def __len__(self):
-        length = 0
-        current_ref = self.head
-        while current_ref:
-            length += 1
-            current_ref = current_ref.next_ref
-        return length
-
-    def __getitem__(self, item):
-        i = 0
-        neg = False
-        if abs(item) > len(self):
-            raise IndexError('double list index out of range')
-        if item >= 0:
-            current_ref = self.head
-        else:
-            neg = True
-            current_ref = self.end
-            item = -item
-            i = 1
-        while i != item:
-            if not neg:
-                current_ref = current_ref.next_ref
+    def _remove(self, item):
+        length = len(self)
+        left = False
+        if item == 0:
+            left = True
+        elif item > 0:
+            if item <= (length-1)//2:
+                left = True
             else:
-                current_ref = current_ref.last_ref
-            i += 1
-        return current_ref.data
+                item -= length-1
+        else:
+            if abs(item) > (length-1)//2:
+                left = True
+            item += length
+        if left:
+            i = 0
+        else:
+            i = length-1
+        current_ref = self.head if left else self.end
+        while current_ref:
+            print(i, item)
+            if i == item:
+                if current_ref.last_ref and current_ref.next_ref:
+                    current_ref.last_ref.next_ref = current_ref.next_ref
+                    current_ref.next_ref.last_ref = current_ref.last_ref
+                else:
+                    if left:
+                        self.head = current_ref.next_ref
+                        if current_ref.next_ref:
+                            current_ref.next_ref.last_ref = None
+                    else:
+                        self.end = current_ref.last_ref
+                        if current_ref.last_ref:
+                            current_ref.last_ref.next_ref = None
+                break
+            else:
+                if left:
+                    current_ref = current_ref.next_ref
+                    i += 1
+                else:
+                    current_ref = current_ref.last_ref
+                    i -= 1
 
     def _append(self, data, left=False):
         new_ref = Reference(data, None)
@@ -177,63 +180,38 @@ class DoubleLinkedList:
     def appendleft(self, data):
         self._append(data, True)
 
-    def _extend(self, data, left=False):
-        if not getattr(data, '__iter__'):
-            raise TypeError('{} is not iterable'.format(type(data)))
-        for item in data:
+    def _extend(self, iterable, left=False):
+        if not getattr(iterable, '__iter__'):
+            raise TypeError('{} is not iterable'.format(type(iterable)))
+        for item in iterable:
             self._append(item, left)
 
-    def extend(self, data):
-        self._extend(data)
+    def extend(self, iterable):
+        self._extend(iterable)
 
-    def extendleft(self, data):
-        self._extend(data, True)
+    def extendleft(self, iterable):
+        self._extend(iterable, True)
 
-    def _pop(self, left=False):
-        if len(self):
-            ref = self.head if left else self.end
-            self.remove(ref.data, left)
-            return ref.data
-        raise IndexError('pop from empty double list')
+    def _pop(self, item=None):
+        length = len(self)
+        if item is None:
+            item = length-1
+        if type(item) != int:
+            raise TypeError('list indices must be integer, not {}'.format(type(item)))
+        if not length:
+            raise IndexError('pop from empty {}'.format(self.structname))
+        if item > length-1:
+            raise IndexError('pop index out of range')
+        return (self[item], self._remove(item))[0]
 
-    def pop(self, i=False):
-        return self._pop(i)
+    def popright(self):
+        return self._pop()
 
     def popleft(self):
-        return self._pop(True)
+        return self._pop(0)
 
-    def remove(self, value, left=False):
-        current_ref = self.head if left else self.end
-        if not len(self):
-            raise IndexError('remove item from empty double list')
-        while current_ref:
-            if current_ref.data == value:
-                if current_ref.last_ref and current_ref.next_ref:
-                    current_ref.last_ref.next_ref = current_ref.next_ref
-                    current_ref.next_ref.last_ref = current_ref.last_ref
-                else:
-                    if left:
-                        self.head = current_ref.next_ref
-                        if current_ref.next_ref:
-                            current_ref.next_ref.last_ref = None
-                    else:
-                        self.end = current_ref.last_ref
-                        if current_ref.last_ref:
-                            current_ref.last_ref.next_ref = None
-                break
-            else:
-                current_ref = current_ref.next_ref if left else current_ref.last_ref
-
-    def reveal(self):
-        current_ref = self.head
-        while current_ref:
-            print(current_ref.reveal())
-            current_ref = current_ref.next_ref
-
-#test = DoubleLinkedList()
-#for x in range(128):
-#    test.append(x)
-
+    def pop(self, item):
+        return self._pop(item)
 
 class Queue:
     def __init__(self, queue_type='f', maxsize=None):
