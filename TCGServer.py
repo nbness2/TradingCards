dded01985bb1ce5735b97939428c0075d955303e
nbness2 <nbness1337@gmail.com>
2016-08-11 17:42:15 -0600

    [ADD] password "command" to sendrecv
    this prompts the client to have text entered in a 'password' like box
    (with the circles\stars for each character)

--- TCGServer.py
+++ TCGServer.py
@@ -6,7 +6,7 @@ from threading import Thread
 from os import walk, makedirs
 
 
-class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+class TCGServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     daemon_threads = True
     allow_reuse_address = True
 
@@ -31,7 +31,7 @@ class UserHandler(socketserver.BaseRequestHandler):
              'login_success': 'Successfully logged in.',
              'invalid_up': 'Invalid Username or Password.',
              'login_activate_register': '(L)ogin, (A)ctivate, or (R)egister: ',
-             'activation_email': 'Welcome, {0}!\n Thank you for registering your account with pyTCG! Your activation code is:\n{1}\n\n\nCheck out the source code for this TCG: https://github.com/nbness2/TradingCards/'
+             'registration_email': 'Welcome, {0}!\n Thank you for registering your account with pyTCG! Your activation code is:\n{1}\n\n\nCheck out the source code for this TCG: https://github.com/nbness2/TradingCards/'
              }
 
     def handle(self):
@@ -46,8 +46,8 @@ class UserHandler(socketserver.BaseRequestHandler):
             elif response == '~':
                 break
             else:
-                send_receive(self.request, 'Invalid choice: '+response, 'p')
-        send_receive(self.request, 'Thank you for choosing pyTCG!', 'p')
+                send_receive(self.request, 'Invalid choice: '+response, 'n')
+        send_receive(self.request, 'Thank you for choosing pyTCG!', 'n')
         self.request.close()
 
     def login(self):
@@ -58,12 +58,13 @@ class UserHandler(socketserver.BaseRequestHandler):
         activated = int(activated)
         if passhash == user_passhash:
             if activated:
-                send_receive(socket, self.message['login_success'], 'p')
+                send_receive(socket, self.message['login_success'], 'n')
             else:
-                send_receive(socket, self.message['not_activated'], 'p')
+                send_receive(socket, self.message['not_activated'], 'n')
                 self.activate(username, passhash)
+            self.menu()
         else:
-            send_receive(socket, self.message['invalid_up'], 'p')
+            send_receive(socket, self.message['invalid_up'], 'n')
 
     def activate(self, username=None, passhash=None):
         socket = self.request
@@ -74,65 +75,46 @@ class UserHandler(socketserver.BaseRequestHandler):
         user_activated = int(user_activated)
         del user_emailhash
         if user_activated:
-            send_receive(socket, self.message['already_activated'], 'p')
+            send_receive(socket, self.message['already_activated'], 'n')
         else:
             activation_code = send_receive(socket, self.message['activation_code'], recvsize=11)
             if passhash == user_passhash and activation_code == user_activation_code:
                 queues['activation'][0].put(username)
-                send_receive(socket, self.message['act_success'], 'p')
+                send_receive(socket, self.message['act_success'], 'n')
             else:
-                send_receive(socket, self.message['invalid_act_code'], 'p')
+                send_receive(socket, self.message['invalid_act_code'], 'n')
 
     def register(self):
-        try:
-            socket = self.request
-            passed = False
-            useremail, password, username = ('', '', '')
-            paramchecks = {}
-            while not passed:
-                if len(paramchecks):
-                    estring = err_str(paramchecks, ['Username', 'Password', 'Email'])
-                    send_receive(socket, estring, 'p', 1)
-                    del estring
-                username = send_receive(socket, self.message['register_username'], recvsize=16)
-                password = send_receive(socket, self.message['register_password'], recvsize=32)
-                useremail = send_receive(socket, self.message['register_email'], recvsize=64)
-                paramchecks = check_details(username, password, useremail)
-                passhash = pyhash.Sha384(password).hexdigest()
-                del password
-                if type(paramchecks) == bool:
-                    passed = True
-            del paramchecks, passed
-            ehash = pyhash.Sha384(useremail.lower()).hexdigest()
-            activation_code = pyhash.Md5(pyrand.randstring(16)).hexdigest[::3]
-            queues['register'][0].put((username, (0, activation_code, passhash, ehash)))
-            emessage = 'Dear {0}, Thank you for registering your account with pyTCG! Your activation code is:\n{1}\n'.format(username, activation_code)
-            email_params = (useremail, emessage, 'pyTCG activation code', email, emailpass, smtpaddr, False)
-            queues['email'][0].put(email_params)
-            del username, activation_code, passhash, ehash,
-            send_receive(socket, self.message['registration_success'], 'p', 1)
-        except Exception as e:
-            write_error(e) #not good practise but if problems do happen i can add exceptions for those cases
+        socket = self.request
+        passed = False
+        useremail, password, username = ('', '', '')
+        paramchecks = {}
+        while not passed:
+            if len(paramchecks):
+                estring = err_str(paramchecks, ['Username', 'Password', 'Email'])
+                send_receive(socket, estring, 'n', 1)
+                del estring
+            username = send_receive(socket, self.message['register_username'], recvsize=16)
+            password = send_receive(socket, self.message['register_password'], stype='p', recvsize=32)
+            print(password)
+            useremail = send_receive(socket, self.message['register_email'], recvsize=64)
+            paramchecks = check_details(username, password, useremail)
+            passhash = pyhash.Sha384(password).hexdigest()
+            del password
+            if type(paramchecks) == bool:
+                passed = True
+        del paramchecks, passed
+        ehash = pyhash.Sha384(useremail.lower()).hexdigest()
+        activation_code = pyhash.Md5(pyrand.randstring(16)).hexdigest[::3]
+        queues['register'][0].put((username, (0, activation_code, passhash, ehash)))
+        emessage = self.message['registration_email'].format(username, activation_code)
+        email_params = (useremail, emessage, 'pyTCG activation code', email, emailpass, smtpaddr, False)
+        queues['email'][0].put(email_params)
+        del username, activation_code, passhash, ehash,
+        send_receive(socket, self.message['registration_success'], 'n', 1)
 
     def menu(self): #cli menu
         pass
-
-
-class LoginContainer(Thread, dict):
-    def __init__(self):
-        dict.__init__(self)
-        Thread.__init__(self)
-
-    def add_sess(self, sessname):
-        self[sessname] = pyhash.Md5(sessname+pyrand.randstring(2))[:16]
-
-    def del_sess(self, sessname):
-        del self[sessname]
-
-
-class Session:
-    def __init__(self, socket):
-        self.socket = socket
 
 
 class QueueWorker(Thread):
@@ -152,12 +134,14 @@ class QueueWorker(Thread):
 
 def send_receive(socket, sendmsg, stype='i', recvsize=1):
     # Sends encoded data + command, returns decoded receive data
-    # p, 0x00 = no input
+    # n, 0x00 = no input
     # i, 0x01 = input
-    commands = {'p': 0x00, 'i': 0x01, 'q': 0x09}
+    # p, 0x02 = password
+    # q, 0x09 = quit
+    commands = {'n': 0x00, 'i': 0x01, 'p': 0x02, 'q': 0x09}
     send_data = str(commands[stype])+sendmsg
     socket.send(send_data.encode())
-    if stype == 'i':
+    if stype in ('i', 'p'):
         recv_data = socket.recv(64).decode()[:recvsize]
         return recv_data
     socket.recv(64)[:1]
@@ -175,7 +159,6 @@ def err_str(errdict, paramorder=()):
 
 def check_details(username=None, password=None, email=None):
     faults = {'Username': [], 'Password': [], 'Email': []}
-
     if password:
         passwordc = regrules.check_password(password)
         del password
@@ -197,7 +180,9 @@ def check_details(username=None, password=None, email=None):
             faults['Email'].append(emailc)
 
     for fault in faults:
-        if len(fault):
+        if len(faults[fault]):
+            print('found fault')
+            print(fault)
             return faults
     return True
 
@@ -236,40 +221,30 @@ def activate_user(username, userdir='users/'):
     return True
 
 
-def write_error(exception):
-    with open('errors.txt', 'w') as errfile:
-        errfile.write(str(exception))
+def write_exception(exception, errdir='errors/'):
+    with open(errdir+'errors.txt', 'a') as err_file:
+        err_file.write(str(exception))
 
 
 def makedir(path):
     makedirs(path, exist_ok=True)
 
-incloginlimit = 5
-inclogintimeout = 600
 
-email, emailpass, smtpaddr = emailinfo.info
+def start_server(ServerClass, server_addr, RequestHandler):
+    server = ServerClass(server_addr, RequestHandler)
+    workers = {queue: QueueWorker(queues[queue]) for queue in queues}
+    for queue in queues:
+        workers[queue].start()
+    server.serve_forever()
 
-HOST = ''
-PORT = 1337
 
-queues = {
+if __name__ == "__main__":
+    queues = {
     'register': [Queue('l'), write_user],
     'activation': [Queue('l'), activate_user],
     'email': [Queue('l'), pyemail.send_email],
     }
-
-meow = True
-
-workers = {queue: QueueWorker(queues[queue]) for queue in queues}
-
-if __name__ == "__main__":
-    server = SimpleServer((HOST, PORT), UserHandler)
-    try:
-        for queue in queues:
-            workers[queue].start()
-        server.serve_forever()
-    except KeyboardInterrupt:
-        sys.exit(0)
-    except Exception as e:
-        print(e)
-        sys.exit(0)
+    email, emailpass, smtpaddr = emailinfo.info
+    HOST = ''
+    PORT = 1337
+    start_server(TCGServer, (HOST, PORT), UserHandler)
